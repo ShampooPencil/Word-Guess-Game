e9ab1b2c0990f6e21f9cb8f6eb63a18a506435d2
DrewDrizzle <aramouni8@gmail.com>
2018-11-10 14:43:27 -0500

    commit

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -21,6 +21,7 @@ var remGuessesId = document.getElementById("remGuesses");
 var lettersGuessedId = document.getElementById("lettersGuessed");
 var wordGuessId = document.getElementById("wordGuess");
 var word = game.guessWord.replace(/\w/g, "_"); //use a RegEx to replace all letters with "_";
+wordGuessId.textContent = word;
 var guessWordsArray;
 var NUM_WINS = document.getElementById("numOfWins");
 var NUM_LOSSES = document.getElementById("numOfLosses");
@@ -30,19 +31,26 @@ var NUM_LETTRS_GUESSD = document.getElementById("numOfLettrsGuessd");
 var guessWordsArray = word.split("");
 var LOWER_CASE = game.guessWord.toLowerCase();
 var Guesses = 15;
-var resartFunction = function(){
+var restartFunction = function(){
     game.guessWord = game.wordsArray[Math.floor(Math.random() * game.wordsArray.length)];
-    Guesses = 16;
+    console.log("NEWGAMEWORD: ", game.guessWord);
+    word = game.guessWord.replace(/\w/g, "_");
+    LOWER_CASE = game.guessWord.toLowerCase();
+    Guesses = 15;
+    NUM_REM_GUESSES.innerHTML = Guesses;
     game.lettersGuessed = Array();
     game.rightLetters = Array();
-    console.log(game.guessWord);
-    console.log(wordGuessId.innerHTML);
+    //console.log(game.guessWord);
+    //console.log(wordGuessId.innerHTML);
     var guessWordsArray = word.split("");
+    wordGuessId.textContent = word;
+    placeHolder = document.getElementById("wordGuess").innerHTML;
+    console.log("NewPlaceHolder: ", placeHolder);
     //LOWER_CASE = game.guessWord.toLowerCase();
     //NUM_REM_GUESSES = Guesses;
 }
-console.log(wordGuessId.innerHTML);
-
+//console.log(wordGuessId.innerHTML);
+var placeHolder = document.getElementById("wordGuess").innerHTML;
 document.onkeyup = function(event){  //Main function. (side note, I think MainFunction works I HOPE)//I checked the cars game and it workd. it also add the var's together etc with it to
    game.userGuess = event.key;
    //LOWER_CASE = game.guessWord.toLowerCase();
@@ -55,33 +63,31 @@ document.onkeyup = function(event){  //Main function. (side note, I think MainFu
     console.log(Guesses);   
     if(Guesses < 1){       //track losses and if Guesses is down to 0, restart the the main function/game.
         game.losses = game.losses + 1;
-        lossesId.textContent = game.losses;
-        resartFunction();
+        lossesId.textContent = "LOSSES: " + game.losses;
+        restartFunction();
     }
     
     var search_;
     var search_ = game.userGuess;
     
-    //console.log(trueOrFalse);//var trueOrNot = (game.userGuess == game.guessWord.charCodeAt(search_)); //if a userGuess pressed on a key that has a letter in the team that was selected, replace "_" to the letter
-    //console.log(game.guessWord[search_] === game.userGuess);
     
-    /*if (game.guessWord(game.guessWord) === game.userGuess) {
-    wordGuessId.innerHTML = wordGuessId + game.userGuess;
-    } */
-    //console.log(game.guessWord[search_].toString() === game.userGuess);
-
     
     for(var i = 0; i < game.guessWord.length; i++){
-        //var makeToString = "";
-        //makeToString = game.guessWord[i].toString();
-        console.log(game.guessWord.charAt() === game.userGuess);
-        if(game.guessWord.charAt(i).toString() === search_){
-            console.log(game.guessWord.charAt(i), search_);
-            word = word.replace("_", search_);
-            wordGuessId.innerHTML = word;
+      
+        //console.log("lower: ", LOWER_CASE);
+        //console.log("GuessWord: ", game.guessWord);
+        //console.log("search: ", search_);
+        if(LOWER_CASE.charAt(i) === search_){
+            //console.log(game.guessWord.charAt(i), search_);
+            //console.log("Substring: ", LOWER_CASE.substr(0, i));
+            placeHolder = placeHolder.substr(0, i) + search_ + placeHolder.substr(i + 1, placeHolder.length);
+            document.getElementById("wordGuess").innerHTML = placeHolder.toUpperCase();
+
+            //console.log("placeHolder: ", placeHolder);
+
             //wordGuessId.innerHTML = wordGuessId.replace("_", game.userGuess);
         }
-        console.log(game.userGuess);
+        //console.log(game.userGuess);
         /*if(game.userGuess.match(/^[A-Za-z]$/)){
             
             if(game.guessWord[i] === game.userGuess){
@@ -94,12 +100,14 @@ document.onkeyup = function(event){  //Main function. (side note, I think MainFu
                 }
             }*/
         }
-        if(game.guessWord === wordGuessId.innerHTML){
+        checkWord = placeHolder.toUpperCase();
+        console.log("END: ", game.guessWord, " & ", checkWord);
+        if(game.guessWord === checkWord){
             game.wins = game.wins + 1;
+            winsId.textContent = "WINS: " + game.wins;
             restartFunction();
         }
-        lettersGuessedId.textContent = game.lettersGuessed;
-        wordGuessId.textContent = word;
+        lettersGuessedId.textContent.innerHTML = game.lettersGuessed;
     
     }
     
